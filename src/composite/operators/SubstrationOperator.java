04411f2aab63d1a31e0f4fa5f1bfecfa37b3cfee
Johannes Goebel <j.goebel@prisma-computer.de>
2017-06-06 11:35:48 +0200

    added missing operators

---
+++ src/composite/operators/SubstrationOperator.java
@@ -0,0 +1,32 @@
+package composite.operators;
+
+import composite.Operator;
+
+/**
+ * Created by johannes on 06.06.17.
+ */
+public class SubstrationOperator extends Operator {
+
+    public void print() {
+        if (first != null && last != null) {
+            System.out.print("(");
+            first.print();
+            System.out.printf("-");
+            last.print();
+            System.out.print(")");
+        } else {
+            throw new IllegalStateException("NO!");
+        }
+    }
+
+
+    @Override
+    public int evaluate() {
+        if (first != null && last != null) {
+            return first.evaluate() + last.evaluate();
+        } else {
+            throw new IllegalStateException("NO!");
+
+        }
+    }
+}
